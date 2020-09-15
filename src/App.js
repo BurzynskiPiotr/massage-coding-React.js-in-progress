7f780082cd60fd83a7b15d589d5ef1921dc2b422
Piotr <51122302+BurzynskiPiotr@users.noreply.github.com>
2020-09-15 12:47:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style/stylesheet.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-panel">
+        <form>
+          <input type="text" placeholder="write your message here" />
+
+          <button>Accept</button>
+        </form>
+        <p className="welcoming-massage">Encode your own message</p>
+      </div>
+      <footer></footer>
     </div>
   );
 }
 
 export default App;
+
+// to do:
+
+// after puting value in input, show button submit
+// animate gif above input
+// after submiting show lower menu
+// ...
+
+// undo the coding section on click
